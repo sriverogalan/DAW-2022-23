16a153554663e732fa9aaccbaea896a4706806ed
Sergi Rivero Galán <56429173+sriverogalan@users.noreply.github.com>
2022-10-09 22:08:12 +0200

    final changes

---
+++ Docker/php/volumes/primervideo/funciones/funciones4.php
@@ -0,0 +1,42 @@
+<html lang="ES">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+    <?php
+        // Varias Matematicas
+        echo (abs(-6.7));
+        echo "<br>";
+
+        echo (max(2, 4, 6, 8, 10));
+        echo "<br>";
+
+        echo (min(2, 4, 6, 8, 10));
+        echo "<br>";
+
+        echo(rand(10, 100));
+        echo "<br>";
+
+        echo(sqrt(9)); // Raiz Cuadrada
+        echo "<br>";
+
+        // Transformar funciones
+
+        echo( decbin("3"));
+        echo "<br>";
+
+        echo( decbin("1587"));
+        echo "<br>";
+
+        echo( dechex("15"));
+        echo "<br>";
+
+        echo( decbin("1011"));
+        echo "<br>";
+
+
+
+
+    ?>
+</body>
+</html>
