db1b08ad7313e9ce3903865436593734746a505e
Sergi Rivero Galán <56429173+sriverogalan@users.noreply.github.com>
2022-10-09 22:10:15 +0200

    little changes

--- Docker/php/volumes/primervideo/index.php
+++ Docker/php/volumes/primervideo/index.php
@@ -1,60 +1,22 @@
-<html lang="ES">
+<html>
 <head>
     <meta charset="utf-8">
 </head>
 <body>
     <?php
-        // Matematicas
-        $nota = 7.213;
-        if (is_int($nota)) {
-            echo "La nota es entera";
-        } elseif (is_float($nota)) {
-            echo "La nota no es entera";
+        setcookie("noexpira",1);
+        setcookie("micookie",2,time()+60);
+
+        setcookie("idioma", "esp");
+
+        if ($_COOKIE["idioma"] == "esp") {
+            echo "Hola";
+        } else {
+            echo "Hello";
         }
 
-        echo "<br>";
-
-        echo number_format("1000000" );
-        echo "<br>";
-        echo number_format("1000000", 2 );
-        echo "<br>";
-        echo number_format("1000000", 2, ",", ".");
-        echo "<br>";
-        echo "<br>";
-
-        // Redondear
-        echo round(0.60);
-        echo "<br>";
-        echo round(0.49);
-        echo "<br>";
-        echo round(0.50);
-        echo "<br>";
-        echo round(-0.51);
-        echo "<br>";
-        echo round(-4.60);
-        echo "<br>";
-        echo round(-4.40669846,2);
-        echo "<br>";
-        echo "<br>";
-        echo round(1.50,0,PHP_ROUND_HALF_UP);
-        echo "<br>";
-        echo round(1.50,0,PHP_ROUND_HALF_DOWN);
-        echo "<br>";
-        echo round(1.50,0,PHP_ROUND_HALF_EVEN);
-        echo "<br>";
-        echo (ceil(0.6));
-        echo "<br>";
-        echo (ceil(5));
-        echo "<br>";
-        echo (ceil(5.1));
-        echo "<br>";
-        echo "<br>";
-        echo (floor(0.6));
-        echo "<br>";
-        echo (floor(5));
-        echo "<br>";
-        echo (floor(5.1));
-        echo "<br>";
+        unset($_COOKIE["noexpira"]);
+        setcookie("noexpira", "", time()-1);
     ?>
 </body>
 </html>
