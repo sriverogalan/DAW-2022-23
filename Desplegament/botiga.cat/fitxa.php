46bb5a1daf69525e759e3592aae8ebb62dc620f9
Sergi Rivero Galán <56429173+sriverogalan@users.noreply.github.com>
2022-10-17 12:51:47 +0200

    Cambio en la localizacion de Desplegament y cambios en la linea

--- Desplegament/botiga.cat/fitxa.php
+++ Desplegament/botiga.cat/fitxa.php
@@ -7,34 +7,29 @@
     <title>Fitxa</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;1,400&display=swap" rel="stylesheet">
 </head>
-<body> 
-    
+<style>
+      body {
+        font-family: 'Roboto Condensed', sans-serif;
+      }
+</style>
+<body>   
 <nav class="navbar navbar-expand-lg bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Botiga.cat</a>
+        <a class="navbar-brand" href="llista.php">Botiga.cat</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="alta-producte.html">Afegir producte</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
+                    <a class="nav-link" href="carreto.php">Carreto</a>
                 </li> 
         </div>
     </div>
@@ -49,15 +44,15 @@ $result = $conn->query($sql); // Execute query
  
 if ($result->num_rows > 0) { // output data of each row
     while($row = $result->fetch_assoc()) { 
-        echo "<div class='col-6'><img src='img/".$row["id"].".jpg' class='col-12'></div>";
-        echo "<div class='col-6 d-flex align-items-center'>";
+        echo "<div class='col-md-6 col-sm-12'><img src='img/".$row["id"].".jpg' class='col-12'></div>";
+        echo "<div class='col-md-6 col-sm-12 d-flex align-items-center'>";
             echo "<div class='col-12 row'>";
                 echo "<h2>".$row["nom"]."</h2>";
                 echo "<p>".$row["descripcio"]."</p>";
                 echo "<p>".$row["preu"]." €</p>";
                 echo "<div class='d-flex justify-content-between'>";
-                    echo "<a href='carreto.php' class='btn btn-primary col-4'>Comprar</a>";
-                    echo "<a href='llista.php' class='btn btn-danger col-4'>Tornar</a>"; 
+                    echo "<a href='carreto.php' class='btn btn-primary col-md-4 col-sm-6'>Comprar</a>";
+                    echo "<a href='llista.php' class='btn btn-danger col-md-4 col-sm-6'>Tornar</a>"; 
                 echo "</div>";
             echo "</div>";
         echo "</div>";
--- Desplegament/botiga.cat/llista.php
+++ Desplegament/botiga.cat/llista.php
@@ -7,11 +7,19 @@
     <title>Botiga</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;1,400&display=swap" rel="stylesheet">
 </head>
+<style>
+      body {
+        font-family: 'Roboto Condensed', sans-serif;
+      }
+</style>
 <body>
 <nav class="navbar navbar-expand-lg bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Botiga.cat</a>
+        <a class="navbar-brand" href="llista.php">Botiga.cat</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -21,18 +29,7 @@
                     <a class="nav-link active" aria-current="page" href="alta-producte.html">Afegir producte</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
+                    <a class="nav-link" href="carreto.php">Carreto</a>
                 </li> 
         </div>
     </div>
@@ -47,7 +44,7 @@ $result = $conn->query($sql); // Execute query
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<a href='fitxa.php?codi=".$row["id"]."' class='col-4 text-center text-decoration-none p-1 border'><img src='img/".$row["id"].".jpg' class='col-12'>";
+        echo "<a href='fitxa.php?codi=".$row["id"]."' class='col-md-4 col-sm-6 text-center text-decoration-none p-1 border'><img src='img/".$row["id"].".jpg' class='col-12'>";
         echo  "<h3>".$row["nom"]."</h3>";
         echo  "<p>".$row["descripcio"]."</p>";
         echo  "<p>".$row["preu"]." € </p><br></a> ";
