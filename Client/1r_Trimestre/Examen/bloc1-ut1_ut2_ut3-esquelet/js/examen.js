cfe14143530c581852d2d1ded85ce3be470162d2
Sergi Rivero Galán <56429173+sriverogalan@users.noreply.github.com>
2022-10-25 12:10:25 +0200

    changes

--- Client/1r_Trimestre/Examen/bloc1-ut1_ut2_ut3-esquelet/js/examen.js
+++ Client/1r_Trimestre/Examen/bloc1-ut1_ut2_ut3-esquelet/js/examen.js
@@ -1,18 +1,16 @@
 function Joc(){
     this.mans = ["pedra", "paper", "tisores"]; 
     this.maMaquina = this.mans[Math.floor(Math.random() * this.mans.length)];  
-    this.maJugador;  
-    this.init = function(){ 
-        this.maJugador = prompt('Introduir "pedra", "paper" o "tisores"')
+    this.maJugador= prompt('Introduir "pedra", "paper" o "tisores"');  
+    this.init = function(){  
         this.maJugador = this.maJugador.toLowerCase();   
         console.log(this.maJugador);
         while(this.mans.indexOf(this.maJugador) === -1){ 
             this.maJugador = prompt('Introduir "pedra", "paper" o "tisores"')
             this.maJugador = this.maJugador.toLowerCase(); 
         }
-        this.checkWin();
-        
-    }  
+        this.checkWin(); 
+    }
     this.checkWin = function(){ 
         if (this.maMaquina === "pedra" && this.maJugador === "paper") {
             alert("Has guanyat!");
@@ -26,7 +24,11 @@ function Joc(){
             alert("Has perdut!");
         }
     }
-}
+    this.pinta = function(){
+        document.getElementById("hand1").innerHTML = this.maMaquina;
+        document.getElementById("hand2").innerHTML = this.maJugador;
+    }
+} 
 
 var joc = new Joc();
 joc.init();
