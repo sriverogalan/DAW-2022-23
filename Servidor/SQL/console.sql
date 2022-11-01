01d1b2e2b0016a3e6de6c4a82264f8fd2628c4de
Sergi Rivero <56429173+sriverogalan@users.noreply.github.com>
2022-11-02 00:15:48 +0100

    functional changes PDO botiga

---
+++ Servidor/SQL/console.sql
@@ -0,0 +1,34 @@
+SELECT * from articles;
+
+
+insert into articles(nom, descripcio, preu) values  ("Pantalons", "Pantalons de verano", 23.50);
+
+insert into articles(nom, descripcio, preu) values  ("Camiseta", "Camiseta de verano", 18.50);
+
+insert into articles(nom, descripcio, preu) values  ("Zapatos", "Zapatos de verano", 43.50);
+
+insert into articles(nom, descripcio, preu) values  ("Camisa", "Camisa de verano", 25.50);
+
+insert into articles(nom, descripcio, preu) values  ("Pantalón", "Pantalón de verano", 20.50);
+
+insert into articles(nom, descripcio, preu) values  ("Blusa", "Blusa de verano", 23.50);
+
+insert into articles(nom, descripcio, preu) values  ("Falda", "Falda de verano", 23.50);
+
+insert into articles(nom, descripcio, preu) values  ("Vestido", "Vestido de verano", 23.50);
+
+insert into articles(nom, descripcio, preu) values  ("Sudadera", "Sudadera de verano", 23.50);
+
+insert into articles(nom, descripcio, preu) values  ("Jersey", "Jersey de verano", 23.50);
+
+insert into articles(nom, descripcio, preu) values  ("Abrigo", "Abrigo de verano", 23.50);
+
+insert into articles(nom, descripcio, preu) values  ("Chaqueta", "Chaqueta de verano", 23.50);
+
+insert into articles(nom, descripcio, preu) values  ("Gorra", "Gorra de verano", 23.50);
+
+insert into articles(nom, descripcio, preu) values  ("Sombrero", "Sombrero de verano", 23.50);
+
+insert into articles(nom, descripcio, preu) values  ("Bufanda", "Bufanda de verano", 23.50);
+
+insert into articles(nom, descripcio, preu) values  ("Calcetines", "Calcetines de verano", 23.50);
