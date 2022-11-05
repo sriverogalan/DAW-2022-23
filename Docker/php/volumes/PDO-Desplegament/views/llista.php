5abab8d23e7fdda39b804a3a99b0ee6cbb1e4230
Sergi Rivero <56429173+sriverogalan@users.noreply.github.com>
2022-11-05 20:51:12 +0100

    llista.php actualizacion

--- Docker/php/volumes/PDO-Desplegament/views/llista.php
+++ Docker/php/volumes/PDO-Desplegament/views/llista.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-include '../views/navbar.php'; 
+include '../views/navbar.php';
 require '../controllers/ProductController.php';
+$articles = getProducts();
 ?>
 <header>
     <title>Llista</title>
@@ -10,12 +11,6 @@ require '../controllers/ProductController.php';
 
 <body>
     <?php 
-    $connection = AccessDatabase::getInstance();
-    $pdo = $connection->getConnection();
-    $sql = $pdo->prepare("SELECT * FROM articles"); // id (tambe es l'id de les imatges + .jpg), nom, descripcio, preu
-    $sql->execute();
-    $articles = $sql->fetchAll(PDO::FETCH_ASSOC);
-
     echo '<div class="container text-center mt-3">';
     echo '<div class="row">';
     if ($articles) {
@@ -40,7 +35,8 @@ require '../controllers/ProductController.php';
     echo '</div>';
     $pdo = null;
     ?>
-</body> 
+</body>
+
 </html>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
