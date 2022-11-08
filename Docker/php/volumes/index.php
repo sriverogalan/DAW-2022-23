ea898c368ec72970fe0f7e17e4ca259d881207a8
Sergi Rivero Galán <56429173+sriverogalan@users.noreply.github.com>
2022-11-08 13:05:12 +0100

    bootstrap changes

--- Docker/php/volumes/index.php
+++ Docker/php/volumes/index.php
@@ -4,7 +4,7 @@
 </head>
 <body>
 <a href="exercicisVideos/index.php">PRIMER VIDEOS PHP</a>
-<a href="PDO-Desplegament/views/llista.php">Llista php</a>
+<a href="PDO-Desplegament/views/llista.php">Llista php - PDO</a>
+<a href="Botiga/index.php">MVC - PHP</a>
 </body>
-</html>
-
+</html> 
