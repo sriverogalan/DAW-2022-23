65a55932c9e6517835c41bd4547a1033a535c72a
Sergi Rivero Galán <56429173+sriverogalan@users.noreply.github.com>
2022-11-17 13:19:47 +0100

    changes

---
+++ Servidor/demoSpring/src/main/java/com/demospring/demospring/Entities/Producte.java
@@ -0,0 +1,21 @@
+package com.demospring.demospring.Entities;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity @Data @NoArgsConstructor
+public class Producte {
+    @Id private int id;
+    private String nom;
+    private String descripcio;
+    private double preu;
+
+    public Producte(String nom, double preu, String descripcio) {
+        this.nom = nom;
+        this.preu = preu;
+        this.descripcio = descripcio;
+    }
+
+}
