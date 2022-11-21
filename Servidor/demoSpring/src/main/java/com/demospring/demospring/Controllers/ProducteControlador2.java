e7658add7a91b2911ed7c216456e7c0dbc1248fa
Sergi Rivero <56429173+sriverogalan@users.noreply.github.com>
2022-11-21 17:37:28 +0100

    changes controller session

--- Servidor/demoSpring/src/main/java/com/demospring/demospring/Controllers/ProducteControlador2.java
+++ Servidor/demoSpring/src/main/java/com/demospring/demospring/Controllers/ProducteControlador2.java
@@ -1,14 +1,15 @@
 package com.demospring.demospring.Controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Controller
 public class ProducteControlador2 {
     @GetMapping("/producte2/salutacions")
     String Salutacions(){
         return "Hola mon";
     }
 
-    @GetMapping("/producte2/")
+    @GetMapping("/producte2")
     String Salutacions2(){
         return "Hola mon";
     }
