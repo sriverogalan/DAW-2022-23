c404aaa47d9cff5abbc5136d567bcc7de5f266cd
Sergi Rivero <56429173+sriverogalan@users.noreply.github.com>
2022-11-28 23:58:35 +0100

    changes channel

--- Client/Segon-bloc/UD6/Ex 4 - Programació IES Manacor TV/Controller/CanalService.js
+++ Client/Segon-bloc/UD6/Ex 4 - Programació IES Manacor TV/Controller/CanalService.js
@@ -1,4 +1,4 @@
-import { Canal } from '../Model/Canal.js';
+import { Canal } from "../Model/Canal.js";
 
 export class CanalService {
   _URL = "https://theteacher.codiblau.com";
@@ -14,14 +14,30 @@ export class CanalService {
   }
 
   async getCanals() {
-    const canalsFetch = await fetch(this._URL + "/exercicis/programacio/info"  );
-    const canals = await canalsFetch.json();  
+    const canalsFetch = await fetch(this._URL + "/exercicis/programacio/info");
+    const canals = await canalsFetch.json();
     const clau = Object.keys(canals);
     const valor = Object.values(canals);
     const canalsArray = [];
     for (let i = 0; i < clau.length; i++) {
       canalsArray.push(new Canal(clau[i], valor[i]));
-    } 
+    }
     return canalsArray;
+  }
+
+  async getProgramacio(canal, hour) {
+    const programacioFetch = await fetch(
+      this._URL + "/exercicis/programacio/tv",
+      {
+        method: "POST",
+        headers: {
+          "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        },
+        credentials: "include",
+        body: `can=${canal}&hour=${hour}`,
+      }
+    );
+    const programacio = await programacioFetch.json();
+    return programacio;
   }
 }
--- Client/Segon-bloc/UD6/Ex 4 - Programació IES Manacor TV/Model/Programa.js
+++ Client/Segon-bloc/UD6/Ex 4 - Programació IES Manacor TV/Model/Programa.js
@@ -1,10 +1,13 @@
 export class Programa {
     
-  constructor(arrayString) {
-    this.nom = arrayString;
+  constructor(name, hour, program, age) {
+    this.nom = name;
+    this.hora = hour;
+    this.programa = program;
+    this.edat = age;
   }
   
   static fromJSON(json) {
-    return new Programa(json);
+    return new Programa(json.nom, json.hora, json.programa, json.edat);
   }
 }
