73a588a3e47b639c1e052753ec9ba789825a4ce2
Sergi Rivero <56429173+sriverogalan@users.noreply.github.com>
2022-12-12 22:13:24 +0100

    Starting with laravel

---
+++ Docker/DockerLaravel/Laravel.md
@@ -0,0 +1,6 @@
+## List all artisan commands:
+
+docker-compose exec myapp php artisan list
+
+## comando laravel para crear un controller
+docker-compose exec myapp php artisan make:controller UserController 
---
+++ Docker/DockerLaravel/my-project/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\CursoController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+
+
+ */ 
+Route::get('/', HomeController::class);  
+
+Route::controller(CursoController::class)->group(function () {
+    Route::get('cursos', 'index');
+    Route::get('cursos/create','create');
+    Route::get('cursos/{curso}', 'show');
+});
+
+/*Route::get('/cursos', [CursoController::class, 'index']);
+Route::get('cursos/create', [CursoController::class, 'create']);
+Route::get('cursos/{curso}', [CursoController::class, 'show']);*/
