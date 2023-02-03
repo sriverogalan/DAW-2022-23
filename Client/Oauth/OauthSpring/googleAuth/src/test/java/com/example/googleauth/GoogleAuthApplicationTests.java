f8057174f3ddf4c160cf59a423650137e5a8c5d9
Sergi Rivero Galán <sriverogalan@gmail.com>
2023-02-03 09:23:33 +0100

    NewAuth

---
+++ Client/Oauth/OauthSpring/googleAuth/src/test/java/com/example/googleauth/GoogleAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.googleauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
