6a131e6ba01ab241f8501ab0ccc622a616f72cc6
Sergi Rivero <56429173+sriverogalan@users.noreply.github.com>
2023-02-13 22:17:31 +0100

    new project

---
+++ Servidor/TreballFinal/DAWSpring/src/main/java/com/daw/spring/DawSpringApplication.java
@@ -0,0 +1,13 @@
+package com.daw.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DawSpringApplication.class, args);
+    }
+
+}
---
+++ Servidor/TreballFinal/DAWSpring/src/test/java/com/daw/spring/DawSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daw.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
